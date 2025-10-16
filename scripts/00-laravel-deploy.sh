83d717b061897edd41726e5468b279ef75ac9920
fin5 <refin555@gmail.com>
2025-10-17 00:17:47 +0700

    Fix: Add file permissions for storage folder to fix 502

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,3 +1,7 @@
+echo "Setting File Permissions..."
+chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
+chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
+
 echo "Running Composer Install..."
 composer install --no-dev --prefer-dist --working-dir=/var/www/html
 
